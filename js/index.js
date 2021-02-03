e809c0ac88528657396e8b29a527405e75b9c0d7
Zhan Yihao <51394067+Zireael19Andre@users.noreply.github.com>
2021-02-04 08:21:08 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementsByName('p')/*从DOM中按元素名称寻找*/
+document.getElementsByClassName()/*从DOM中按类名称寻找*/
+document.getElementById()/*从DOM中按ID寻找*/
+'p'.innerHTML()/*元素内的网页*/
+'p'.style()/*元素的风格化*/
+'a'.setAttribute()/*设置元素的属性*/
+'a'.removeAttribute()/*移除元素的属性*/
+
